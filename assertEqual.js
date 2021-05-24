db6a7dcdb77ecd6e0f17a5f3b66eb16b21c9a9e4
Soliloquy <ibrahia@ucalgary.ca>
2021-05-24 22:38:59 +0000

    Assertion with emojis

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + " Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log(String.fromCodePoint(0x274C) + " Assertion Failed: " + actual + " != " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(5, 1);
