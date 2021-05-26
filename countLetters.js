5ddb813aba361ca5ce941502778da8124a052193
Soliloquy <ibrahia@ucalgary.ca>
2021-05-26 21:34:44 +0000

    Object iteration

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(String.fromCodePoint(0x274C) + ` Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const result = {};
+  for (num of str) {
+    if (num === " ") {
+      continue;
+    }
+    if (result[num]) {
+      result[num] += 1;
+    } else {
+      result[num] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(countLetters("Lighthouse in the house"));
