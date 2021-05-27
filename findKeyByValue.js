f00ee9edf08c64c166fcd43d5315242623d65e6c
Soliloquy <ibrahia@ucalgary.ca>
2021-05-27 01:00:33 +0000

    Iterating Objects

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(String.fromCodePoint(0x274C) + ` Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let found;
+  for (const key in obj) {
+    if (value === obj[key]) {
+      return key;
+    } else {
+      found = false;
+    }
+  }
+  if (!found) {
+    return undefined;
+  }
+}
+
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
