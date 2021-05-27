dcf7e47448d6fbe6c57f106c5686bad982dcbc30
Soliloquy <ibrahia@ucalgary.ca>
2021-05-27 21:12:18 +0000

    callback with nested objects

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + ` Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(String.fromCodePoint(0x274C) + ` Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  // console.log(Object.keys(object))
+  for (let key of Object.keys(object)) {
+    // console.log(object[key])
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+let results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(results, 'noma')
