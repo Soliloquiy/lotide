75abb2a2171cbdf0481e9ed01756c94d267e1ce9
Soliloquy <ibrahia@ucalgary.ca>
2021-05-31 19:35:44 +0000

    Changed modules

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) + ` Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(String.fromCodePoint(0x274C) + ` Assertion Failed: ${actual} != ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([6]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+module.exports = head;
